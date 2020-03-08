0973f2da6a15149737d6d1efb1faab9acacddafc
GSThecar <thecar3774@gmail.com>
2020-03-08 23:53:20 +0900

    Initial commit

---
+++ KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by 이덕화 on 2020/03/08.
+//  Copyright © 2020 GSTheCar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
